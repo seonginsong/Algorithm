7a84389885c208e8e2952b772547394087174cb2
adultCode <as45655@gmail.com>
2025-07-14 16:39:59 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 784 ms, Memory: 235924 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		
+		for(int i=0; i<T; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			bw.write((N+M)+"\n");
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
