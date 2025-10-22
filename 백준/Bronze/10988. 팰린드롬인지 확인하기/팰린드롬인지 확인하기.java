29256b216952e4225cc22186f91aec9528c143f3
adultCode <as45655@gmail.com>
2025-10-22 23:05:30 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 168 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        String reversed = new StringBuilder(s).reverse().toString();
+
+        if (s.equals(reversed)) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+        sc.close();
+    }
+}
