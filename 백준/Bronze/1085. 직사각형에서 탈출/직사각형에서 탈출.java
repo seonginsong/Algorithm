922d07bcf292fae15beda73e35d45c12ef4ce695
adultCode <as45655@gmail.com>
2025-11-28 20:18:47 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 168 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
+        int min = Math.min(Math.min(x, w - x), Math.min(y, h - y));
+
+        System.out.println(min);
+    }
+}
