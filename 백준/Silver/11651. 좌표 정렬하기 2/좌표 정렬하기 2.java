7982ea1c57ca522f3a63a75f054b0ff4d99dda9a
adultCode <as45655@gmail.com>
2026-01-16 00:00:51 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 572 ms, Memory: 51172 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken()); // x
+            arr[i][1] = Integer.parseInt(st.nextToken()); // y
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            if (a[1] == b[1]) {
+                return a[0] - b[0];
+            }
+            return a[1] - b[1];
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i][0]).append(' ')
+              .append(arr[i][1]).append('\n');
+        }
+
+        System.out.print(sb.toString());
+    }
+}
