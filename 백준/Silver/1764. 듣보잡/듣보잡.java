84e1e95c7bf1f1de7507fec4ac68431077a48984
adultCode <as45655@gmail.com>
2026-01-30 15:37:44 +0900

    [Silver IV] Title: 듣보잡, Time: 252 ms, Memory: 26388 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Set<String> set = new HashSet<>();
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        for (int i = 0; i < M; i++) {
+            String name = br.readLine();
+            if (set.contains(name)) {
+                result.add(name);
+            }
+        }
+
+        Collections.sort(result);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(result.size()).append('\n');
+        for (String name : result) {
+            sb.append(name).append('\n');
+        }
+
+        System.out.print(sb.toString());
+    }
+}
