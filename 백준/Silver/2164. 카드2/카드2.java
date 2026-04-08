942663dc94e2dd35a5a0e70e8db36bdb5fcad908
adultCode <as45655@gmail.com>
2026-04-08 13:32:37 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 45452 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= n; i++) q.offer(i);
+
+        while (q.size() > 1) {
+            q.poll();
+            q.offer(q.poll());
+        }
+
+        System.out.println(q.peek());
+    }
+}
